8ac2155f015e6fd0fb68589889f3ba67ac3e729b
Farhan452002 <farhan.ahmed1@mail.citytech.cuny.edu>
2024-11-14 15:19:18 -0500

    RLab Part 3: Start

--- RLab.js
+++ RLab.js
@@ -1,3 +1,4 @@
+// RLab Part 2:
 console.log("RLab Part 2:");
 let csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 let rows = csvData.split("\n");
@@ -11,3 +12,28 @@ for(let i = 0;i<rows.length;i++){
     data[i] = rows[i].split(",");
 }
 console.log(data);
+
+// RLab Part 3:
+console.log("RLab Part 3:");
+let csvRows = []; // Array of objects
+
+for(let i=1;i<rows.length;i++){
+    let csvObj = {id,name,occupation,age};
+    for(let j=0;j<columns;j++){
+        switch(j){
+            case 0:
+                csvObj.id = data[i][j];
+                break;
+            case 1:
+                csvObj.name = data[i][j];
+                break;
+            case 2:
+                csvObj.occupation = data[i][j];
+                break;
+            case 3:
+                csvObj.age = data[i][j];
+                break;                   
+        }
+        csvRows.push(csvObj);
+    }
+}
